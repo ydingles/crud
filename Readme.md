811d84b59fae70aa6d05e6b132dff73bcbb902f6
Yvonne Ingles <ydingles@hotmail.com>
2015-10-29 01:00:43 -0700

    changes

--- Readme.md
+++ Readme.md
@@ -1,4 +1,9 @@
+http://students.washington.edu/ydingles/info343/crud
 Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+N/a
 Did you complete any advanced extensions to this challenge? If so, what?
+No
 Approximately how many hours did it take you to complete this challenge?
+24 hours
 Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+I had trouble putting together the reviews to be displayed onto the page.
--- js/review.js
+++ js/review.js
@@ -1,52 +1,47 @@
 $(document).ready(function(){
 
-
 // Initialize Parse app
-Parse.initialize("1yhdhqrslDBUveCWFvu1hE5sXH4ciuhIseBej08n", 
-	"aC5L5leSdCxZT6rdFICtD1IvtnRvRW3pziqmHcJW");
+	Parse.initialize("1yhdhqrslDBUveCWFvu1hE5sXH4ciuhIseBej08n", "aC5L5leSdCxZT6rdFICtD1IvtnRvRW3pziqmHcJW");
 
-var Review = Parse.Object.extend('Review');
+	var Review = Parse.Object.extend('Review');
 
+	//set raty
 	$('#stars').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images'});
 
-
-// function to set data
+	// function to set data
 	$('form').submit(function() {
 
-	// Create a new instance of your Music class 
-	// For each input element, set a property of your new instance equal to the input's value
-	// After setting each property, save your new instance back to your database
+		// Create new instance of Review
+		var newReview = new Review();
+		//	Set a property 'stars' equal to user stars
+		newReview.set('stars', $('#stars').raty('score'));
+		// Set a property 'title' equal to a user title
+		newReview.set('title', $('#title').val()); 
+		// Set a property 'name' equal to the user name
+		newReview.set('name', $('#name').val()); 
+	 	// Set a property 'reviewtext' equal to user review
+		newReview.set('reviewtext', $('#reviewtext').val()); 
+		// Save instance of review to parse
+		newReview.save();
 
-	// Create a new instance of your Music class 
-	var newReview = new Review();
-	//	Set a property 'stars' equal to user stars
-	newReview.set('stars', $('#stars').raty('score'));
-	// Set a property 'title' equal to a user title
-	newReview.set('title', $('#title').val()); 
-	// Set a property 'name' equal to the user name
-	newReview.set('name', $('#name').val()); 
- 	// Set a property 'reviewtext' equal to user review
-	newReview.set('reviewtext', $('#reviewtext').val()); 
-	// Save instance of review to see it on parse.com!
-	newReview.save();
+		// clear the form
+		$('#stars').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images'});
+		$('#title').val('');
+		$('#name').val('');
+		$('#reviewtext').val('');
 
-	// clear the form
-	$('#stars').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images'});
-	$('#title').val('');
-	$('#name').val('');
-	$('#reviewtext').val('');
+		getData();
 
-	return false // prevents a new page from loading
-});
+		return false // prevents a new page from loading
+	});
 
-//var starnumber = $('#stars').raty('score'));
 
-// function to run a query
+	// function to run a query
 	var getData = function() {
 		var query = new Parse.Query(Review);
 		query.notEqualTo('stars', null);
-//		query.notEqualTo('title','');
-//		query.notEqualTo('name','');
+		// query.notEqualTo('title','');
+		// query.notEqualTo('name','');
 		query.notEqualTo('reviewtext','');
 		query.find({
 			success:function(results) {
@@ -56,51 +51,65 @@ var Review = Parse.Object.extend('Review');
 
 	}
 
-// function to build list
+	// function to build list
 	var buildList = function(data) {
-		$('ol').empty();
+		$('ul').empty();
+
+		//$('#listreviews').empty();
 
 		data.forEach(function(d) {
 			addItem(d);
 		})
 	}
 
-// function to take item and add to screen
+	// function to take item and add to screen
 	var addItem = function(item) {
 		var stars = item.get('stars');
 		var title = item.get('title');
 		var name = item.get('name');
 		var reviewtext = item.get('reviewtext');
+		var voteUP = item.get('voteUP');
+		var voteDOWN = item.get('voteDOWN');
 	
-		var div = $('<div id="reviewstar">' + $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly: true, score: stars}) + '</div>');
-		
-		//var reviewstar = $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly: true, score: stars})
-		//var li = $('<li>' + '<div id="reviewstar">' + $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images'}) + '</div>' + title + name + reviewtext + '</li>');
-		//var div = $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly:true, score: stars})
-		
-		//var reviewstar = $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly: true, score: stars})
-		
-		var li = $('<li>' +  title + " " + name + " " + reviewtext + '</li>')
-		
-		//var li = $('<li>' + "<div id=starsgo>" + "</div>" + title + '</li>')
-		//$('#starsgo').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly: true, score: stars});
+		var staraty =  $('#reviewstar').raty({path:'http://students.washington.edu/ydingles/info343/crud/lib/images', readOnly: true, score: stars});
 
-		var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+		var div = $('<div id="reviewstar">'+'</div>');
+		var li = $('<li>' + '<h3>' + title + '</h3>' + "by " + name + '<br>' + reviewtext + '<br></br>' + 'Was this review helpful? ' + voteUP + ' YES! ' + voteDOWN + ' NO! ' + '</li>')
 
+
+		// button to remove a review
+		var button = $('<button class="btn-danger btn-xs" style="float:right"><span class="glyphicon glyphicon-remove"></span></button>')
 		button.click(function() {
 			item.destroy({
 				success:getData
+			})
 		})
-	})
 
-		li.prepend(div);
-		li.append(button);
-		$('ol').append(li);
+		// button to record up votes
+		var up = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-arrow-up"></span></button>')
+    	up.click(function() {
+      		item.increment("voteUP");
+      			item.save();
+      			getData();     
+    	})
+
+    	// button to record down votes
+		var down = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-arrow-down"></span></button>')
+    	down.click(function() {
+      		item.increment("voteDOWN");
+      			item.save();
+      			getData();     
+    	})
+
+
+
+		$('ul').append(li).prepend(div);
+		li.prepend(staraty);
+		li.prepend(button);
+		li.append(up).append(down);
 
 	}
 
 getData();
-
-
 
 })
